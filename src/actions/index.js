cf6ad96541100a3720cf4d2927e648d85c24a677
Mayank <gamblergamer9@gmail.com>
2020-07-30 22:31:26 +0530

    update project

---
+++ src/actions/index.js
@@ -0,0 +1,13 @@
+import jsonPlaceholder from "../apis/jsonPlaceholder";
+
+// export const fetchPosts = () => {
+//   return async (dispatch, getState) => {
+//     const response = jsonPlaceholder.get('/posts');
+//     dispatch({type: 'FETCH_POSTS', payload: response})
+//   };
+// };
+
+export const fetchPosts = () => async dispatch => {
+  const response = jsonPlaceholder.get('/posts');
+  dispatch({type: 'FETCH_POSTS', payload: response})
+};
