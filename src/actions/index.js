bc78e75e86121536bb1b482650f0344630d1b4c7
Mayank <gamblergamer9@gmail.com>
2020-07-30 23:28:00 +0530

    update Post List data fetch

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,6 +8,6 @@ import jsonPlaceholder from "../apis/jsonPlaceholder";
 // };
 
 export const fetchPosts = () => async dispatch => {
-  const response = jsonPlaceholder.get('/posts');
-  dispatch({type: 'FETCH_POSTS', payload: response})
+  const response = await jsonPlaceholder.get('/posts');
+  dispatch({type: 'FETCH_POSTS', payload: response.data})
 };
